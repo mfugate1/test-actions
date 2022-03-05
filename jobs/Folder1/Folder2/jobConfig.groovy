663d6e8025b22a26b5ed8776e351fb2d719fd495
Mike Fugate <mfugate@carnegielearning.com>
2022-03-05 09:56:39 -0500

    Create jobConfig.groovy

---
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -0,0 +1,20 @@
+String jobPath = new File(__FILE__).parent
+
+pipelineJob("${jobPath - 'jobs/'}".replaceAll("/", "-")) {
+  definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url("https://github.dev.carnegielearning.com/CarnegieLearning/cli-jenkins.git")
+                        credentials("github-enterprise-credentials")
+                    }
+                    branch("dev")
+                }
+                lightweight()
+                scriptPath("jobs/Mathia/Publish-Typescript-From-DTOs/Jenkinsfile")
+            }
+        }
+    }
+    
+}
