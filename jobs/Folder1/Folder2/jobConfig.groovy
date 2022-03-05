d4f56996352cda723cd06492c877706ad0c1ef3e
Mike Fugate <mfugate@carnegielearning.com>
2022-03-05 17:19:35 -0500

    Update jobConfig.groovy

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -1,13 +1,3 @@
 String jobPath = new File(__FILE__).parent
 
-println(__FILE__)
-
-pipelineJob("${jobPath - 'jobs/'}".replaceAll("/", "-")) {
-  definition {
-        cps {
-            sandbox(true)
-            script(readFileFromWorkspace("testJenkinsfile"))
-        }
-    }
-    
-}
+println(hudson.model.Executor.currentExecutor().getCurrentWorkspace().getBaseName())
