3c79a45c4b194fb3589f40674eaee66457c74188
Mike Fugate <mfugate@carnegielearning.com>
2022-03-05 11:09:05 -0500

    Update jobConfig.groovy

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -4,18 +4,9 @@ println(__FILE__)
 
 pipelineJob("${jobPath - 'jobs/'}".replaceAll("/", "-")) {
   definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url("https://github.dev.carnegielearning.com/CarnegieLearning/cli-jenkins.git")
-                        credentials("github-enterprise-credentials")
-                    }
-                    branch("dev")
-                }
-                lightweight()
-                scriptPath("jobs/Mathia/Publish-Typescript-From-DTOs/Jenkinsfile")
-            }
+        cps {
+            sandbox(true)
+            script(readFileFromWorkspace("testJenkinsfile"))
         }
     }
     
