395c6500e059d41b595dcf83849700f844cfe9ea
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 08:31:39 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -1,56 +1,7 @@
 import static PipelineJobUtils.addGitScmDefinition
+import static PipelineJobUtils.addEcsParameters
 
 job = pipelineJob ("TEST-JOB-666")
 
-addGitScmDefinition (
-    job: job
-)
-
-String ecsMemoryScript = """\
-    List possibleValues = [512] + (1..30).collect{it * 1024}
-    int cpu = ecsCpu as Integer
-    return possibleValues.findAll{it >= 2 * cpu && it <= 8 * cpu}"""
-
-job.with {
-    parameters {
-        choiceParameter {
-            name("ecsCpu")
-            choiceType("PT_SINGLE_SELECT")
-            randomName("")
-            filterable(false)
-            filterLength(0)
-            script {
-                groovyScript {
-                    script {
-                        sandbox(true)
-                        script("['256', '512', '1024:selected', '2048', '4096']")
-                    }
-                    fallbackScript {
-                        script("'Error getting cpu values'")
-                        sandbox(true)
-                    }
-                }
-            }
-        }
-        cascadeChoiceParameter {
-            name("ecsMemory")
-            choiceType("PT_SINGLE_SELECT")
-            randomName("")
-            filterable(false)
-            filterLength(0)
-            referencedParameters("ecsCpu")
-            script {
-                groovyScript {
-                    script {
-                        sandbox(true)
-                        script(ecsMemoryScript)
-                    }
-                    fallbackScript {
-                        script("'Error getting memory values'")
-                        sandbox(true)
-                    }
-                }
-            }
-        }
-    }
-}
+addGitScmDefinition (job: job)
+addEcsParameters(job: job)
--- jobs/shared/PipelineJobUtils.groovy
+++ jobs/shared/PipelineJobUtils.groovy
@@ -21,4 +21,59 @@ class PipelineJobUtils {
             }
         }
     }
+
+    static void addEcsParameters(Map config) {
+        String ecsMemoryScript = """\
+            List possibleValues = [512] + (1..30).collect{it * 1024}
+            int cpu = ecsCpu as Integer
+            return possibleValues.collect{it >= 2 * cpu && it <= 8 * cpu}""".stripIndent()
+
+        job.with {
+            parameters {
+                string {
+                    name("ecsImage")
+                    defaultValue(config.image ?: "jenkins/inbound-agent:alpine")
+                }
+                choiceParameter {
+                    name("ecsCpu")
+                    choiceType("PT_SINGLE_SELECT")
+                    randomName("")
+                    filterable(false)
+                    filterLength(0)
+                    script {
+                        groovyScript {
+                            script {
+                                sandbox(true)
+                                script("['256', '512', '1024:selected', '2048', '4096']")
+                            }
+                            fallbackScript {
+                                script("'Error getting cpu values'")
+                                sandbox(true)
+                            }
+                        }
+                    }
+                }
+                cascadeChoiceParameter {
+                    name("ecsMemory")
+                    choiceType("PT_SINGLE_SELECT")
+                    randomName("")
+                    filterable(false)
+                    filterLength(0)
+                    referencedParameters("ecsCpu")
+                    script {
+                        groovyScript {
+                            script {
+                                sandbox(true)
+                                script(ecsMemoryScript)
+                            }
+                            fallbackScript {
+                                script("'Error getting memory values'")
+                                sandbox(true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
