05b8fcfc610e537ffd872eedc75629909ec26847
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 07:56:19 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -8,23 +8,10 @@ addGitScmDefinition (
 
 job.with {
     parameters {
-        choiceParameter {
-            name("testparam")
-            choiceType("PT_SINGLE_SELECT")
-            randomName("")
-            filterable(false)
-            filterLength(0)
-            script {
-                groovyScript {
-                    script {
-                        sandbox(true)
-                        script("return ['256', '512', '1024:selected', '2048', '4096']")
-                    }
-                    fallbackScript {
-                        script("return ['Error getting cpu values']")
-                        sandbox(true)
-                    }
-                }
+        activeChoiceParameter ("ecsCpu") {
+            choiceType("SINGLE_SELECT")
+            groovyScript {
+                script("return ['256', '512', '1024:selected', '2048', '4096']")
             }
         }
     }
