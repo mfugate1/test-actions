0c7daa0f42bcdc3496ba8de228e001888d694cba
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 07:40:06 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -1,7 +1,17 @@
-import static addGitScmDefinition
+import static PipelineJobUtils.addGitScmDefinition
 
 job = pipelineJob ("TEST-JOB-666")
 
 addGitScmDefinition (
     job: job
 )
+
+job.with {
+    parameters {
+        choice {
+            name("testparam")
+            choices(["one", "two"])
+            defaultValue("two")
+        }
+    }
+}
--- jobs/shared/PipelineJobUtils.groovy
+++ jobs/shared/PipelineJobUtils.groovy
@@ -1,21 +1,22 @@
 import javaposse.jobdsl.dsl.Job
 
-    
-static void addGitScmDefinition (Map config) {
-    String repoUrl = config.repoUrl ?: "https://github.com/mfugate1/test-actions"
+class PipelineJobUtils {
+    static void addGitScmDefinition (Map config) {
+        String repoUrl = config.repoUrl ?: "https://github.com/mfugate1/test-actions"
 
-    config.job.with {
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url(repoUrl)
+        config.job.with {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url(repoUrl)
+                            }
+                            branch(config.branch ?: "main")
                         }
-                        branch(config.branch ?: "main")
+                        lightweight()
+                        scriptPath(config.scriptPath ?: "Jenkinsfile")
                     }
-                    lightweight()
-                    scriptPath(config.scriptPath ?: "Jenkinsfile")
                 }
             }
         }
