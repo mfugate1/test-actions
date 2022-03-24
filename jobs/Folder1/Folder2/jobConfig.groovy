e8adad992c6316f34cffd706ca121b3356d9a037
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 10:22:03 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -4,4 +4,13 @@ import static PipelineJobUtils.addEcsParameters
 job = pipelineJob ("TEST-JOB-666")
 
 addGitScmDefinition (job: job)
+
+job.with {
+    parameters {
+        string {
+            name("testparam")
+        }
+    }
+}
+
 addEcsParameters(job: job, cpu: "2048", memory: "8192")
