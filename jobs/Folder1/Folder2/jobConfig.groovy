2eb76d0d65e721966994a89bf4a887c28f099372
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 08:27:41 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -9,7 +9,7 @@ addGitScmDefinition (
 String ecsMemoryScript = """\
     List possibleValues = [512] + (1..30).collect{it * 1024}
     int cpu = ecsCpu as Integer
-    return possibleValues.collect{it >= 2 * cpu && it <= 8 * cpu}"""
+    return possibleValues.findAll{it >= 2 * cpu && it <= 8 * cpu}"""
 
 job.with {
     parameters {
