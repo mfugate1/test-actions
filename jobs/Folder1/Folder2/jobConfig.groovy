e7c2dda296dee3224ae8af930fd04b6d35b0db69
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 07:58:21 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -8,13 +8,13 @@ addGitScmDefinition (
 
 job.with {
     parameters {
-        activeChoiceParam('CHOICE-1') {
-            description('Allows user choose from multiple choices')
+        activeChoiceParam("CHOICE-1") {
+            description("Allows user choose from multiple choices")
             filterable()
-            choiceType('SINGLE_SELECT')
+            choiceType("SINGLE_SELECT")
             groovyScript {
-                script('["choice1", "choice2"]')
-                fallbackScript('"fallback choice"')
+                script("['choice1', 'choice2']")
+                fallbackScript("'fallback choice'")
             }
         }
     }
