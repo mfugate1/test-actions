4c3f7fbfa153813bcbdad834bcebf1410ab3967c
Mike Fugate <mfugate@carnegielearning.com>
2022-03-24 08:24:28 -0400

    test

--- jobs/Folder1/Folder2/jobConfig.groovy
+++ jobs/Folder1/Folder2/jobConfig.groovy
@@ -6,18 +6,49 @@ addGitScmDefinition (
     job: job
 )
 
+String ecsMemoryScript = """\
+    List possibleValues = [512] + (1..30).collect{it * 1024}
+    int cpu = ecsCpu as Integer
+    return possibleValues.collect{it >= 2 * cpu && it <= 8 * cpu}"""
+
 job.with {
     parameters {
-        activeChoiceParam ("ecsCpu") {
-            choiceType("SINGLE_SELECT")
-            groovyScript {
-                script {
-                    sandbox(true)
-                    script("['256', '512', '1024:selected', '2048', '4096']")
+        choiceParameter {
+            name("ecsCpu")
+            choiceType("PT_SINGLE_SELECT")
+            randomName("")
+            filterable(false)
+            filterLength(0)
+            script {
+                groovyScript {
+                    script {
+                        sandbox(true)
+                        script("['256', '512', '1024:selected', '2048', '4096']")
+                    }
+                    fallbackScript {
+                        script("'Error getting cpu values'")
+                        sandbox(true)
+                    }
                 }
-                fallbackScript {
-                    script("'Error getting cpu values'")
-                    sandbox(true)
+            }
+        }
+        dynamicReferenceParameter {
+            name("ecsMemory")
+            choiceType("PT_SINGLE_SELECT")
+            randomName("")
+            filterable(false)
+            filterLength(0)
+            referencedParameters("ecsCpu")
+            script {
+                groovyScript {
+                    script {
+                        sandbox(true)
+                        script(ecsMemoryScript)
+                    }
+                    fallbackScript {
+                        script("'Error getting memory values'")
+                        sandbox(true)
+                    }
                 }
             }
         }
